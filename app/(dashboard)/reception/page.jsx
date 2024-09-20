8612ccb9ee8b6dcd4cff032f6a0b6cdc083c0caa
merrick2000 <menongbemerrick@gmail.com>
2024-09-20 17:17:16 +0100

    updates tab names for reception

--- app/(dashboard)/reception/page.jsx
+++ app/(dashboard)/reception/page.jsx
@@ -35,108 +35,106 @@ const columns = [
 
 const buttons = [
   {
-    title: "Departamento Contábil",
+    title: "Guia de impostos",
     icon: "heroicons-outline:home",
-  },
-  {
-    title: "Departamento Tributário",
-    icon: "heroicons-outline:user",
   },
   {
     title: "Departamento Pessoal",
     icon: "heroicons-outline:chat-alt-2",
   },
-  
+  {
+    title: "Demonstrações financeiras",
+    icon: "heroicons-outline:user",
+  },
 ];
-const rows =[
+const rows = [
   {
-      employee: "Andrea Gonçalves",
-      position: "Ger. Atendimento",
-      type: "Salário mensal",
-      competence: "March/2024",
-      gross: "R$ 2,086.00",
-      amountPaid: "R$ 1,891.24"
+    employee: "Andrea Gonçalves",
+    position: "Ger. Atendimento",
+    type: "Salário mensal",
+    competence: "March/2024",
+    gross: "R$ 2,086.00",
+    amountPaid: "R$ 1,891.24",
   },
   {
-      employee: "Janilson Borges",
-      position: "Atendente",
-      type: "Adiantamento",
-      competence: "March/2024",
-      gross: "R$ 1,520.00",
-      amountPaid: "R$ 608.00"
+    employee: "Janilson Borges",
+    position: "Atendente",
+    type: "Adiantamento",
+    competence: "March/2024",
+    gross: "R$ 1,520.00",
+    amountPaid: "R$ 608.00",
   },
   {
-      employee: "Joaquim Paula",
-      position: "Ger. Financeiro",
-      type: "Adiantamento",
-      competence: "March/2024",
-      gross: "R$ 2,410.00",
-      amountPaid: "R$ 684.00"
+    employee: "Joaquim Paula",
+    position: "Ger. Financeiro",
+    type: "Adiantamento",
+    competence: "March/2024",
+    gross: "R$ 2,410.00",
+    amountPaid: "R$ 684.00",
   },
   {
-      employee: "Marcelo Leite",
-      position: "Ger. Logística",
-      type: "Salário mensal",
-      competence: "March/2024",
-      gross: "R$ 2,700.00",
-      amountPaid: "R$ 2,477.18"
+    employee: "Marcelo Leite",
+    position: "Ger. Logística",
+    type: "Salário mensal",
+    competence: "March/2024",
+    gross: "R$ 2,700.00",
+    amountPaid: "R$ 2,477.18",
   },
   {
-      employee: "Susi Lazarini",
-      position: "Empacotador",
-      type: "Salário mensal",
-      competence: "March/2024",
-      gross: "R$ 1,470.00",
-      amountPaid: "R$ 1,358.88"
+    employee: "Susi Lazarini",
+    position: "Empacotador",
+    type: "Salário mensal",
+    competence: "March/2024",
+    gross: "R$ 1,470.00",
+    amountPaid: "R$ 1,358.88",
   },
   {
-      employee: "Marcos Alves",
-      position: "Atendente",
-      type: "Adiantamento",
-      competence: "March/2024",
-      gross: "R$ 1,520.00",
-      amountPaid: "R$ 608.00"
+    employee: "Marcos Alves",
+    position: "Atendente",
+    type: "Adiantamento",
+    competence: "March/2024",
+    gross: "R$ 1,520.00",
+    amountPaid: "R$ 608.00",
   },
   {
-      employee: "Bruna Pimentel",
-      position: "Aux. Logística",
-      type: "Adiantamento",
-      competence: "March/2024",
-      gross: "R$ 1,141.20",
-      amountPaid: "R$ 564.80"
+    employee: "Bruna Pimentel",
+    position: "Aux. Logística",
+    type: "Adiantamento",
+    competence: "March/2024",
+    gross: "R$ 1,141.20",
+    amountPaid: "R$ 564.80",
   },
   {
-      employee: "Marcos Alves",
-      position: "Atendente",
-      type: "Adiantamento",
-      competence: "March/2024",
-      gross: "R$ 1,520.00",
-      amountPaid: "R$ 608.00"
+    employee: "Marcos Alves",
+    position: "Atendente",
+    type: "Adiantamento",
+    competence: "March/2024",
+    gross: "R$ 1,520.00",
+    amountPaid: "R$ 608.00",
   },
   {
-      employee: "Bruna Pimentel",
-      position: "Aux. Logística",
-      type: "Adiantamento",
-      competence: "March/2024",
-      gross: "R$ 1,141.20",
-      amountPaid: "R$ 564.80"
+    employee: "Bruna Pimentel",
+    position: "Aux. Logística",
+    type: "Adiantamento",
+    competence: "March/2024",
+    gross: "R$ 1,141.20",
+    amountPaid: "R$ 564.80",
   },
   {
-      employee: "Marcos Alves",
-      position: "Atendente",
-      type: "Adiantamento",
-      competence: "March/2024",
-      gross: "R$ 1,520.00",
-      amountPaid: "R$ 608.00"
-  }
-]
+    employee: "Marcos Alves",
+    position: "Atendente",
+    type: "Adiantamento",
+    competence: "March/2024",
+    gross: "R$ 1,520.00",
+    amountPaid: "R$ 608.00",
+  },
+];
 const TabAccrodain = () => {
   return (
     <div className="grid xl:grid-cols-1 grid-cols-1 gap-6">
-      
       <Card title="">
         <Tab.Group>
-        <Tab.List className=" space-x-0 rtl:space-x-reverse flex">
+          <Tab.List className=" space-x-0 rtl:space-x-reverse flex">
             {buttons.map((item, i) => (
               <Tab className=" " as={Fragment} key={i}>
                 {({ selected }) => (
@@ -184,7 +182,6 @@ const TabAccrodain = () => {
           </Tab.Panels>
         </Tab.Group>
       </Card>
-     
     </div>
   );
 };
