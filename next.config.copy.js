f2e669d655cb4cd627d3e18c18f5c33040f2397f
merrick2000 <menongbemerrick@gmail.com>
2024-09-22 20:44:47 +0100

    github pages ci

--- next.config.copy.js
+++ next.config.copy.js
@@ -1,28 +1,22 @@
 /** @type {import('next').NextConfig} */
 const env = require("./config/env");
-
 const nextConfig = {
   env: {
     API_BASE_URL: process.env.NEXT_PUBLIC_API_URL,
     DEBUG_MODE: process.env.DEBUG_MODE === "true",
+    // Traitement dynamique des variables
     ENVIRONMENT: process.env.NODE_ENV,
     IS_PRODUCTION: process.env.NODE_ENV === "production",
   },
   experimental: {
     appDir: true,
   },
-  output: "export",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/compta-dash" : "",
-  basePath: process.env.NODE_ENV === "production" ? "/compta-dash" : "",
-  images: {
-    unoptimized: true,
-  },
-  // Configuration du proxy (si nécessaire)
+  //output: "standalone",
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`, // Utilise l'URL de l'API définie dans les variables d'environnement
       },
     ];
   },
